f82847e40858e5b436b9d5f1f7138a7418b2c696
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-11-13 15:34:38 +0530

    initial commit

---
+++ cmake/StaticAnalyzers.cmake
@@ -0,0 +1,6 @@
+find_program(CLANGTIDY clang-tidy-16)
+if(CLANGTIDY)
+  set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
+else()
+  message(SEND_ERROR "clang-tidy requested but executable not found")
+endif()
