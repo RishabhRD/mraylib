a10887f642fb42a3429492aecdc07556b1a5cea5
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-11-17 00:11:51 +0530

    ray to include direction

--- include/ray.hpp
+++ include/ray.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
+#include "direction.hpp"
 #include "point.hpp"
-#include "ray.hpp"
-#include "vector.hpp"
 #include <concepts>
 
 namespace mrl {
 
 struct ray_t {
   point3 origin;
-  vec3 direction;
+  direction_t direction;
 
-  constexpr point3 at(double t) const { return origin + t * direction; }
+  constexpr point3 at(double t) const { return origin + t * direction.val(); }
 };
 
 } // namespace mrl
