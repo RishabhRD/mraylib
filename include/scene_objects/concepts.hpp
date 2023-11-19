1201887677671a515c9544c1b80b7487750f0eaf
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-11-19 23:26:50 +0530

    scene object concept added

--- include/scene_objects/concepts.hpp
+++ include/scene_objects/concepts.hpp
@@ -15,4 +15,7 @@ concept Hittable = requires(T const &h, ray_t const &ray, point3 const &point) {
   //   - t should be the minimum possible value for which ray intersects object
   { hit(h, ray) } -> std::same_as<std::optional<hit_record_t>>;
 };
-}
+
+template <typename T>
+concept SceneObject = Hittable<T>;
+} // namespace mrl
