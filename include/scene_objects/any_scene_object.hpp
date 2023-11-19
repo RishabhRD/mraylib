332914f3c9c1cbf4ac8ec56dd13ba646d04b2198
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-11-19 23:54:15 +0530

    added type-erased scene object

---
+++ include/scene_objects/any_scene_object.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "hit_record.hpp"
+#include "ray.hpp"
+#include "scene_objects/concepts.hpp"
+#include <memory>
+#include <optional>
+namespace mrl {
+class any_scene_object {
+public:
+  template <Hittable T>
+  any_scene_object(T x) : self_(std::make_shared(model_t<T>(std::move(x)))) {}
+
+  friend std::optional<hit_record_t> hit(any_scene_object const &obj,
+                                         ray_t const &ray) {
+    return obj.self_->hit(ray);
+  }
+
+private:
+  struct concept_t {
+    virtual ~concept_t() = default;
+    virtual std::optional<hit_record_t> hit(ray_t const &ray) const = 0;
+  };
+
+  template <Hittable T> struct model_t final : concept_t {
+    T hittable;
+    model_t(T h_arg) : hittable(std::move(h_arg)){};
+
+    std::optional<hit_record_t> hit(ray_t const &ray) const override {
+      return hit(hittable, ray);
+    }
+  };
+
+  std::shared_ptr<concept_t const> self_;
+};
+
+std::optional<hit_record_t> hit(any_scene_object const &obj, ray_t const &ray,
+                                point3 const &point);
+} // namespace mrl
