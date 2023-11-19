893ad4400f5429284f9d7ba15bbebdb082624e42
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-11-20 02:34:39 +0530

    fixed any_scene_object

--- include/hit_record.hpp
+++ include/hit_record.hpp
@@ -2,10 +2,17 @@
 
 #include "direction.hpp"
 #include "point.hpp"
+#include <ostream>
 namespace mrl {
 struct hit_record_t {
   double t;
   point3 hit_point;
   direction_t normal;
+
+  friend std::ostream &operator<<(std::ostream &os, hit_record_t const &rec) {
+    os << "{ t : " << rec.t << " , hit_point : " << rec.hit_point
+       << " , normal : " << rec.normal << " }";
+    return os;
+  }
 };
 } // namespace mrl
--- include/scene_objects/any_scene_object.hpp
+++ include/scene_objects/any_scene_object.hpp
@@ -9,25 +9,29 @@
 namespace mrl {
 class any_scene_object {
 public:
-  template <SceneObject T>
-  any_scene_object(T x) : self_(std::make_shared(model_t<T>(std::move(x)))) {}
+  // TODO: Using SceneObject here is creating problem to use it with
+  // scene_object_list with any algorithm that depends on SceneObject.
+  //
+  // template <SceneObject T>
+  template <typename T>
+  any_scene_object(T x) : self_(std::make_shared<model_t<T>>(std::move(x))) {}
 
   friend std::optional<hit_record_t> hit(any_scene_object const &obj,
                                          ray_t const &ray) {
-    return obj.self_->hit(ray);
+    return obj.self_->hit_mem(ray);
   }
 
 private:
   struct concept_t {
     virtual ~concept_t() = default;
-    virtual std::optional<hit_record_t> hit(ray_t const &ray) const = 0;
+    virtual std::optional<hit_record_t> hit_mem(ray_t const &ray) const = 0;
   };
 
   template <SceneObject T> struct model_t final : concept_t {
     T hittable;
     model_t(T h_arg) : hittable(std::move(h_arg)){};
 
-    std::optional<hit_record_t> hit(ray_t const &ray) const override {
+    std::optional<hit_record_t> hit_mem(ray_t const &ray) const override {
       return hit(hittable, ray);
     }
   };
