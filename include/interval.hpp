35336e0589f685ddeaee24c51417453c4ab9ee08
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-12-30 14:48:21 +0530

    fixed interval class

--- include/interval.hpp
+++ include/interval.hpp
@@ -7,14 +7,18 @@ struct interval_t {
   double min;
   double max;
 
+  constexpr interval_t(double min_, double max_) : min{min_}, max{max_} {}
+
+  constexpr interval_t()
+      : interval_t{std::numeric_limits<double>::infinity(),
+                   -std::numeric_limits<double>::infinity()} {}
+
   constexpr bool contains(double x) const { return min <= x && x <= max; }
 
   constexpr bool surrounds(double x) const { return min < x && x < max; }
 };
 
-constexpr static interval_t empty_interval{
-    std::numeric_limits<double>::infinity(),
-    -std::numeric_limits<double>::infinity()};
+constexpr static interval_t empty_interval{};
 
 constexpr static interval_t universe{-std::numeric_limits<double>::infinity(),
                                      std::numeric_limits<double>::infinity()};
