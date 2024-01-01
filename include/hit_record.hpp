2a174a6fd4833f70a05958be9da329a6372a63c1
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-01-01 14:11:54 +0530

    fixed the operator<< problem

--- include/hit_record.hpp
+++ include/hit_record.hpp
@@ -3,10 +3,22 @@
 #include "direction.hpp"
 #include "materials/scatter_record.hpp"
 #include "point.hpp"
-#include "std/optional.hpp"
+#include <optional>
 #include <ostream>
 
 namespace mrl {
+// TODO: there should be generic << for optional<T>
+// But somehow that is not working
+inline std::ostream &operator<<(std::ostream &os,
+                                std::optional<scatter_record_t> const &e) {
+  if (e) {
+    os << *e;
+  } else {
+    os << "nullopt";
+  }
+  return os;
+}
+
 struct hit_record_t {
   double t;
   point3 hit_point;
