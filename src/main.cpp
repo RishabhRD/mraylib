608ec38476b9b3a0d462119979ef1cad9e232c68
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-01-01 13:11:10 +0530

    removed the unnneded function

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "camera/camera_orientation.hpp"
 #include "color.hpp"
 #include "direction.hpp"
-#include "hit_record.hpp"
 #include "image/in_memory_image.hpp"
 #include "image/ppm/ppm_utils.hpp"
 #include "image_renderer.hpp"
@@ -33,72 +32,6 @@
 #include <ios>
 #include <iostream>
 #include <stdexec/execution.hpp>
-
-struct count_hook {
-  int zero = 0;
-  int neg = 0;
-  int pos = 0;
-  int nil = 0;
-
-  void invoke_hit(mrl::ray_t const &ray,
-                  std::optional<mrl::hit_record_t> const &h) {
-    if (h) {
-      auto const n = (*h).normal.val();
-      auto const d = ray.direction.val();
-      auto const res = mrl::dot(d, n);
-      if (res == 0)
-        ++zero;
-      else if (res < 0)
-        ++neg;
-      else
-        ++pos;
-    } else {
-      ++nil;
-    }
-  }
-};
-
-void real() {
-  using any_object = mrl::any_object_t<mrl::inline_scheduler>;
-  mrl::inline_scheduler sch;
-  mrl::aspect_ratio_t ratio{16, 9};
-  auto img_width = 600;
-  auto img_height = mrl::image_height(ratio, img_width);
-  mrl::camera_t camera{
-      .focus_distance = 3.4,
-      .vertical_fov = mrl::degrees(20),
-      .defocus_angle = mrl::degrees(10.0),
-  };
-
-  mrl::camera_orientation_t camera_orientation{
-      .position = {-2, 2, 1},
-      .direction = mrl::point3(0, 0, -1) - mrl::point3(-2, 2, 1),
-      .up_dir = mrl::calc_up_dir({0, 1, 0},
-                                 mrl::point3(0, 0, -1) - mrl::point3(-2, 2, 1)),
-  };
-
-  auto sun_material = mrl::lambertian_t{mrl::color_t{1, 1, 0.0}};
-  auto air = mrl::dielectric{1};
-  auto ground_material = mrl::lambertian_t{mrl::color_t{0.3, 0.3, 0.3}};
-  auto left_material = mrl::dielectric{2};
-  auto right_material = mrl::fuzzy_metal_t{mrl::color_t{0.3, 0.3, 0.3}, 0.2};
-  auto center_material = mrl::lambertian_t{mrl::color_t{0.6, 0.6, 0.3}};
-  auto sun = mrl::sphere_obj_t{0.1, {1, 1, -1.3}, sun_material};
-  auto center_sphere = mrl::sphere_obj_t{0.5, {0, 0, -1}, center_material};
-  auto left_sphere = mrl::sphere_obj_t{0.5, {-1, 0, -1}, left_material};
-  auto left_sphere_1 = mrl::sphere_obj_t{0.3, {-1, 0, -1}, air};
-  auto right_sphere = mrl::sphere_obj_t{0.5, {1, 0, -1}, right_material};
-  auto big_sphere = mrl::sphere_obj_t{100, {0, -100.5, -1}, ground_material};
-
-  std::vector<any_object> world{center_sphere, big_sphere, right_sphere,
-                                left_sphere,   sun,        left_sphere_1};
-
-  mrl::in_memory_image img{img_width, img_height};
-
-  mrl::img_renderer_t renderer(camera, camera_orientation, sch);
-  stdexec::sync_wait(renderer.render(world, img));
-  mrl::write_ppm_img(std::cout, img);
-}
 
 void real_img() {
   auto const num_threads = std::thread::hardware_concurrency();
