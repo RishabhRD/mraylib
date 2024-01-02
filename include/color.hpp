1c30b7b5c0713da7771739a132545b417d261ed0
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-01-03 05:19:56 +0530

    fix bug of from rgb

--- include/color.hpp
+++ include/color.hpp
@@ -74,6 +74,8 @@ constexpr color_t operator/(color_t a, double b) { return a /= b; }
 // Precondition:
 //   - r, g, b >= 0 && r, g, b <= 255
 constexpr color_t from_rgb(int r, int g, int b) {
+  if (r == 0 && g == 0 && b == 0)
+    return {0, 0, 0};
   vec3 tmp = normalize(vec3{static_cast<double>(r), static_cast<double>(g),
                             static_cast<double>(b)});
   return {tmp.x, tmp.y, tmp.z};
