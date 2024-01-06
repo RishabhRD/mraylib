b39449ccee6604342479a45ffc37c3a786014025
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-01-06 13:10:03 +0530

    write without gamma function

--- include/image/ppm/ppm_utils.hpp
+++ include/image/ppm/ppm_utils.hpp
@@ -9,6 +9,11 @@ inline auto write_ppm_color_str(std::ostream &os, color_t color) {
   os << r << ' ' << g << ' ' << b << '\n';
 }
 
+inline auto write_ppm_color_str_without_gamma(std::ostream &os, color_t color) {
+  auto [r, g, b] = to_rgb(color);
+  os << r << ' ' << g << ' ' << b << '\n';
+}
+
 inline auto convert_to_ppm_str(color_t color) {
   std::stringstream ss;
   write_ppm_color_str(ss, color);
@@ -23,6 +28,18 @@ void write_ppm_img(std::ostream &os, Image const &img) {
   for (int y = 0; y < height(img); ++y) {
     for (int x = 0; x < width(img); ++x) {
       write_ppm_color_str(os, pixel_at(img, x, y));
+    }
+  }
+}
+
+template <RandomAccessImage Image>
+void write_ppm_img_without_gamma(std::ostream &os, Image const &img) {
+  os << "P3\n";
+  os << width(img) << ' ' << height(img) << '\n';
+  os << "\n255\n";
+  for (int y = 0; y < height(img); ++y) {
+    for (int x = 0; x < width(img); ++x) {
+      write_ppm_color_str_without_gamma(os, pixel_at(img, x, y));
     }
   }
 }
