c703e8706e133a7e25c4acc8022eb2ca7711dd55
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-02-11 12:20:45 +0530

    metal to support textures

--- include/materials/metal.hpp
+++ include/materials/metal.hpp
@@ -10,37 +10,62 @@
 #include "materials/scatter_info.hpp"
 #include "normal.hpp"
 #include "ray.hpp"
+#include "textures/concepts.hpp"
+#include "textures/solid_color.hpp"
 #include "vector.hpp"
 #include <optional>
 
 namespace mrl {
-struct metal_t {
-  color_t albedo;
+template <typename Texture> struct metal_t {
+  using texture_type = Texture;
+  texture_type albedo;
+
+  metal_t(color_t color) : albedo(solid_color_texture{color}) {}
+  metal_t(texture_type albedo_arg) : albedo(std::move(albedo_arg)) {}
 };
 
-template <DoubleGenerator Generator>
-constexpr std::optional<scatter_info_t> scatter(metal_t const &material,
-                                                scattering_context const &ctx,
-                                                generator_view<Generator>) {
+metal_t(color_t) -> metal_t<solid_color_texture>;
+template <typename Texture> metal_t(Texture) -> metal_t<Texture>;
+
+template <DoubleGenerator Generator, Texture<Generator> texture_t>
+constexpr std::optional<scatter_info_t>
+scatter(metal_t<texture_t> const &material, scattering_context const &ctx,
+        generator_view<Generator> rand) {
+  auto color =
+      texture_color(material.albedo, ctx.scaling_2d, ctx.hit_point, rand);
   auto const &ray = ctx.ray;
   auto normal = normal_dir(ctx.normal, ray.direction);
   auto ray_dir = reflection_dir(ray.direction, normal);
   ray_t scattered_ray{.origin = ctx.hit_point, .direction = ray_dir};
   return scatter_info_t{
       .scattered_ray = scattered_ray,
-      .attenuated_color = material.albedo,
+      .attenuated_color = color,
   };
 }
 
-struct fuzzy_metal_t {
-  color_t albedo;
+template <typename Texture> struct fuzzy_metal_t {
+  using texture_type = Texture;
+
+  texture_type albedo;
   double fuzz_factor;
+
+  fuzzy_metal_t(color_t color, double fuzz)
+      : albedo(solid_color_texture{color}), fuzz_factor(fuzz) {}
+
+  fuzzy_metal_t(texture_type albedo_arg, double fuzz)
+      : albedo(std::move(albedo_arg)), fuzz_factor(fuzz) {}
 };
 
-template <DoubleGenerator Generator>
+template <typename Texture>
+fuzzy_metal_t(Texture, double) -> fuzzy_metal_t<Texture>;
+fuzzy_metal_t(color_t, double) -> fuzzy_metal_t<solid_color_texture>;
+
+template <DoubleGenerator Generator, Texture<Generator> texture_t>
 constexpr std::optional<scatter_info_t>
-scatter(fuzzy_metal_t const &material, scattering_context const &ctx,
+scatter(fuzzy_metal_t<texture_t> const &material, scattering_context const &ctx,
         generator_view<Generator> rand) {
+  auto color =
+      texture_color(material.albedo, ctx.scaling_2d, ctx.hit_point, rand);
   auto const &ray = ctx.ray;
   auto normal = normal_dir(ctx.normal, ray.direction);
   auto ray_dir = reflection_dir(ray.direction, normal);
@@ -52,7 +77,7 @@ scatter(fuzzy_metal_t const &material, scattering_context const &ctx,
     return std::nullopt;
   return scatter_info_t{
       .scattered_ray = scattered_ray,
-      .attenuated_color = material.albedo,
+      .attenuated_color = color,
   };
 }
 } // namespace mrl
